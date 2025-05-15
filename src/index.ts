601f00469334b291ccc3261680a7a4f0553bb2f3
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-05-15 14:34:37 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as actionsCore from '@actions/core';
 
+const input = actionsCore.getInput('custom-input');
+
 console.log('Input:', actionsCore.getInput('custom-input'));
 
-actionsCore.setOutput('custom-output', 'Test123');
+actionsCore.setOutput('custom-output', input);
