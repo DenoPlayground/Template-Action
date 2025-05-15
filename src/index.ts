aaa99932994eacb453ff5820876d5fe8f877e5e7
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-05-15 14:57:00 +0200

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import * as actionsCore from '@actions/core';
 
-const input = actionsCore.getInput('custom-input');
+const customTextInput = actionsCore.getInput('custom-text-input');
+const customListInput = actionsCore.getInput('custom-list-input');
+const customKeyValueInput = actionsCore.getInput('custom-key-value-input');
 
-console.log('Input:', actionsCore.getInput('custom-input'));
+console.log('custom-text-input:', customTextInput);
+console.log('custom-list-input:', customListInput);
+console.log('custom-key-value-input:', customKeyValueInput);
 
-actionsCore.setOutput('custom-output', input);
+actionsCore.setOutput('custom-text-output', 'Test123');
