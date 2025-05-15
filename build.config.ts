0075d6ca5f9cd6cb7870fdb78ad71ff07053539f
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-05-15 13:59:26 +0200

    Refactor

--- build.config.ts
+++ build.config.ts
@@ -1,10 +1,10 @@
-import * as esbuild from 'https://deno.land/x/esbuild@v0.19.12/mod.js';
+import * as esbuild from '@esbuild';
 import { green } from 'https://deno.land/std@0.211.0/fmt/colors.ts';
 import { parseArgs } from 'https://deno.land/std@0.211.0/cli/parse_args.ts';
 
 const args = parseArgs<{
   watch: boolean | undefined,
-  develope: boolean | undefined,
+  develop: boolean | undefined,
   logLevel: esbuild.LogLevel
 }>(Deno.args);
 
@@ -13,9 +13,9 @@ console.log('Build process started.');
 const tsConfig : esbuild.BuildOptions = {
   allowOverwrite: true,
   logLevel: args.logLevel ?? 'info',
-  legalComments: args.develope ? 'inline' : 'none',
+  legalComments: args.develop ? 'inline' : 'none',
   color: true,
-  minify: !args.develope ?? true,
+  minify: !args.develop ?? true,
   outfile: './dist/bundle.min.js',
   entryPoints: [
     './src/index.ts'
